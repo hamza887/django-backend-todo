c8b05ca61e9b16b7ab30024bd47de10d25ff94f3
hamza887 <hamzabutt887@hotmail.com>
2021-07-30 12:32:20 -0700

    doing migration

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Todo(models.Model):
   title= models.CharField(max_length=100)
   description= models.CharField(max_length=300)
+
+
+  def __str__(self):
+      return self.title
